f3b02d2c29fdd0cf1ae2236249a1036aa06b8713
krishivkatariya <krishivkatariya8116@gmail.com>
2026-02-09 11:25:17 +0000

    Fix Django URL configuration and add polls to INSTALLED_APPS

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from polls import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.index, name="home"),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
